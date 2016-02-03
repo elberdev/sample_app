984089e66f1d98d5f31e7847b83c10abd281a533
elberdev <elberdev@gmail.com>
2016-02-03 09:44:04 -0500

    wrote tests for page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | The Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | The Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | The Ruby on Rails Tutorial Sample App"
   end
 end
