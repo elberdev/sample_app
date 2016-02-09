e064676e875b2817811539dbca72caedbaccb85a
elberdev <elberdev@gmail.com>
2016-02-08 22:51:01 -0500

    commented application_helper.rb with some details of Ruby syntax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,16 @@
+# Rails automatically imports this module in our other application files
+# so we have access to any helper methods defined here anywhere
 module ApplicationHelper
 
   # Here we define custom helper functions. We are trying to solve here the
   # problem of the leading '|' in our prior base title
   # Returns the full title on a per-page basis
-  def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
+
+  def full_title(page_title = '')                     # this has an optional argument. the default value is ''
+    base_title = "Ruby on Rails Tutorial Sample App"  # defining a local variable
+    if page_title.empty?                              # if optional argument is empty, return base title
       base_title
-    else
+    else                                              # otherwise return a combination of base title and page title with " | " in between
       page_title + " | " + base_title
     end
   end
