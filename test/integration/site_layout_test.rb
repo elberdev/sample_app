594074f4680350c2cf7120c301929add83f3fab5
elberdev <elberdev@gmail.com>
2016-02-13 17:33:38 -0500

    added tests for ApplicationHelper so we could use the full_title method in our SiteLayoutTest

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -18,5 +18,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+
+    # get signup path
+    get signup_path
+
+    # check title of signup page
+    assert_select "title", full_title("Sign up")
   end
 end
