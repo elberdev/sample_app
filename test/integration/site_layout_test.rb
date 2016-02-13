a6f492556d75dfc94a070994862fed6d6ffb6e1d
elberdev <elberdev@gmail.com>
2016-02-13 15:54:05 -0500

    added integration tests for layout links @ test/integration/site_layout_test.rb

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+# this test class helps us validate all the links in the home page
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+
+    # get the root path
+    get root_path
+
+    # verify that the correct page is loaded
+    assert_template 'static_pages/home'
+
+    # test all the links with method assert_select
+    # this first one has two links to test since there is also a picture
+    # that is linked to the root in addition to the regular link
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+end
