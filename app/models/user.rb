ad0b9d99b1dbef9f65373341becff80783cc99c4
elberdev <elberdev@gmail.com>
2016-02-17 11:51:46 -0500

    added lenght validation for User name and User email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   # this is the method that is called when we test using @user.valid?
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
