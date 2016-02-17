65a76bc8f477dffc5c81f286d68a4b731a16545c
elberdev <elberdev@gmail.com>
2016-02-17 11:04:05 -0500

    added rudimentary validation to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  # this is the method that is called when we test using @user.valid?
+  validates :name, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # this method creates a User instance variable for us to test
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  # this method tests if the user instance variable is valid according to our constraints
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
 end
