12d1ee92e9a1c23ce04550af121b36f3e4d53934
elberdev <elberdev@gmail.com>
2016-02-20 16:16:05 -0500

    fixed syntax error in valid email test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -35,7 +35,7 @@ class UserTest < ActiveSupport::TestCase
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                           first.last@foo.jp alice+bob@baz.cn]
-    valid_addresses each do |valid_address|
+    valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
