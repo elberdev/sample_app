383018bb4040c5c13814ca423a23781bd35f2113
elberdev <elberdev@gmail.com>
2016-02-20 17:08:38 -0500

    deleted fixtures on text/fixtures/users.yml to simplify passing user tests, also added a callback block in app/models/user.rb to make the email lowercase before saving to the db, to prevent db from mistakenly creating unique indices due to case-sensitivity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+
+  # this callback method makes sure all emails are converted to lowercase BEFORE
+  # being saved to the database. Some database adapters use case-sensitive indices
+  # but we will prevent that from happening by using the before_save() method.
+  before_save { self.email = email.downcase } # the word self is optional on the right side
+
   # this is the method that is called when we test using @user.valid?
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
