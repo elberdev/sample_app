5f82f0fb3d167d1e8aaa9e9a1cd0b0072030264a
elberdev <elberdev@gmail.com>
2016-02-25 19:26:08 -0500

    defined create action in app/controllers/users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,26 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    #@user = User.new(params[:user])
+    #not final implementation! this is dangerous because it passes
+    #all parameters to User.new, allowing for malicious attacks
+    #passing in this general hash is no longer the preferred method.
+    #see private method below for a more specific and safer implementation
+
+    @user = User.new(user_params)
+
+    if @user.save
+      #handle successful save
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
