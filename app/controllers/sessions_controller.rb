5324fea587aae01babb1930c5c907980db414adb
elberdev <elberdev@gmail.com>
2016-03-03 07:20:49 -0500

    wrote create action in app/controllers/sessions_controller.rb for basic authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log user in and redirect to user's show page
+    else
+      # create an error message and render new session page again
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
