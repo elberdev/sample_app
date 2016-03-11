419d72997cc02b6bb72993c76456b4c131fbb1d1
elberdev <elberdev@gmail.com>
2016-03-11 12:56:38 -0500

    added log_in_as method in test/test_helper.rb to simulate users logging into our app for testing. The log in method is different depending on whether the test is an integration test or not

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,12 +9,35 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
-  # Add more helper methods to be used by all tests here...
-
   # Returns true if a test user is logged in. Method name is not logged_in? so as
   # to not be confused with the app/helpers/sessions_helper.rb method of that name.
   def is_logged_in?
     !session[:user_id].nil?
   end
-  
+
+  # Logs in a test user
+  def log_in_as(user, options = {})
+
+    # these variables are assigned a default value if the hashes aren't present
+    password    = options[:password] || 'password'
+    remember_me = options[:remember_me] || '1'
+
+    # log in method will be different depending on whether it's an integration test
+    # or not.
+    if integration_test?
+      post login_path, session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+    # Returns true inside an integration test
+    def integration_test?
+      defined?(post_via_redirect)
+    end
+
 end
