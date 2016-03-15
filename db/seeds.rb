6399aa0958c5adf5b799a9ea9ada6759fa9906b0
elberdev <elberdev@gmail.com>
2016-03-15 08:58:53 -0400

    added sample users in db/seeds.rb, reset database and then seeded the database with those users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# create example user
+User.create!(name: "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+# create 99 more of them using Faker and some default values
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
