7e1a605e81d0085930b84aa82ffff0c92f19f9ad
elberdev <elberdev@gmail.com>
2016-03-19 08:45:32 -0400

    created tests for layout links for after a user logs in

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,7 +3,14 @@ require 'test_helper'
 # this test class helps us validate all the links in the home page
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
+
+    # before login
 
     # get the root path
     get root_path
@@ -18,11 +25,27 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
 
     # get signup path
     get signup_path
 
     # check title of signup page
     assert_select "title", full_title("Sign up")
+
+    # after login
+    
+    log_in_as(@user)
+    get root_path
+
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+
   end
 end
