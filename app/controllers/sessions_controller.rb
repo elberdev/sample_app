f499eac2cf4e50500d7bd52b6b0a549303d0a043
elberdev <elberdev@gmail.com>
2016-03-20 12:32:24 -0400

    modified sessions_controller.rb to only log in user if they have already been activated, otherwise flash a warning and redirect to main page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,22 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      # log user in and redirect to user's show page
-      # log_in is a sessions_helper method
-      log_in @user
-      # the remember helper method will install a session cookie in the user's machine
-      # if the user checks off the remember_me checkbox
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      # session_helper method
-      redirect_back_or(@user)
+      # if user has been activated, log in, remember if applicable and redirect
+      if user.activated?
+        # log user in and redirect to user's show page
+        # log_in is a sessions_helper method
+        log_in @user
+        # the remember helper method will install a session cookie in the user's machine
+        # if the user checks off the remember_me checkbox
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        # session_helper method
+        redirect_back_or(@user)
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       # create an error message and render new session page again
       flash.now[:danger] = 'Invalid email/password combination' #sorta
