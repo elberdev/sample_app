7074ae9de065111fffe3235a690222b4eedcc751
elberdev <elberdev@gmail.com>
2016-03-20 11:26:22 -0400

    customized mailer config in config/environments/development.rb and customized account activation method in test/mailers/previews/user_mailer_preview.rb to allow for email previews

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,13 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
-    UserMailer.account_activation
+    # user first user in the database
+    user = User.first
+    # because the activation token is virtual, the user doesn't have one stored.
+    # we must create it
+    user.activation_token = User.new_token
+    # test the activation email
+    UserMailer.account_activation(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
