95a6f270674302dee18617041814ca423d6392a0
elberdev <elberdev@gmail.com>
2016-03-21 20:53:45 -0400

    genereated new password resets controller and created routes in confib.rb for its actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -15,6 +19,8 @@ Rails.application.routes.draw do
   resources :users
   # this will create an account activations named route but only for the edit action
   resources :account_activations, only: [:edit]
+  # create RESTful routes for the password resets actions
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
