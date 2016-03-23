c4757aad33074f55e5bcc65791ac751c11f1c081
elberdev <elberdev@gmail.com>
2016-03-23 15:27:13 -0400

    wrote test for microposts in test/models/micropost_test.rb

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+    # This code is not idiomatically correct, but I'll learne the correct
+    # way later.
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
 end
