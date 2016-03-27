2e472cd51976e895a38ac240596bf5a1d55d35a2
elberdev <elberdev@gmail.com>
2016-03-27 07:56:30 -0400

    added explanatory comments to users_profile_test.rb

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -13,7 +13,12 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     # the full_title() method comes from application_helper.rb
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
+    # this is the nesting syntax for tags. It's looking for an img tag inside
+    # an h1 block
     assert_select 'h1>img.gravatar'
+    # response.body contains the full html of the page.
+    # assert_select requires an accompanying html tag.
+    # assert_match is does not.
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination'
     @user.microposts.paginate(page: 1).each do |micropost|
