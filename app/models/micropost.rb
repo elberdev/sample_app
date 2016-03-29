ecc295ebf37dafbf133a995204408207fc5cf053
elberdev <elberdev@gmail.com>
2016-03-29 18:10:13 -0400

    added picture size validation in micropost model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,16 @@ class Micropost < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  # notice validate keyword instead of validates when doing a custom validation
+  validate :picture_size
+
+  private
+
+    # Validates the size of an uploaded picture
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
+
 end
