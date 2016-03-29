71eeb786cefef15f47013e5b5435e3fd3cfd3542
elberdev <elberdev@gmail.com>
2016-03-28 22:45:19 -0400

    added uploader to micropost.rb model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,8 @@ class Micropost < ActiveRecord::Base
   # the default scope sets the order in which entries will be pulled from the
   # database. In this case it's most recent to older
   default_scope -> { order(created_at: :desc) }
+  # mount_uploader is a CarrieWave method to associate an image with a model
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
