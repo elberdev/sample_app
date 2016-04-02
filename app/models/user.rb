ad40d4fa5a43e4068ff2dc77e999cdb26a969b1e
elberdev <elberdev@gmail.com>
2016-04-02 12:45:30 -0400

    added passive_relationship associations to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,13 @@ class User < ActiveRecord::Base
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
   # customized name of attribute to "following" instead of the awkward "followeds".
   # As long as we tell rails the source is "followed", then we're good.
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships
 
   # the key word attr_accessor creates both a setter and a getter method for our
   # virtual variables.
